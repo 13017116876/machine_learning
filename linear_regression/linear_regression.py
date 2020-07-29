3d569fbe6bfda7a52a63ed61d6186d10d1737480
Barton <897041564@qq.com>
2020-07-29 18:45:56 +0800

    modefy linear_regression.py

---
+++ linear_regression/linear_regression.py
@@ -0,0 +1,71 @@
+"""
+流程：
+    1.准备数据、参数、迭代次数
+    2.迭代，将数据输入到模型
+    3.计算该点的损失函数梯度
+    4.调整参数
+    5.计算损失
+    6.进行下次迭代
+"""
+import numpy as np
+import math
+class LinearRegression(object)
+
+    def init(self,iter):        
+        # self.w = np.random.randn(1,1)
+        self.b = np.random.randn(1,1) # 创建b
+        self.iter = iter
+        self.lr = 0.01
+        self.loss_arr = []
+
+    def train(self,x,y):
+        """
+        self.train_data = train_data #[[1,3],[2.6],[3,8]]
+        self.train_lable = train_label#[4,5,6]
+        """
+        if len(x) != len(y):
+            raise "data length is not equal with label length"
+        self.x=x # x的特征数据
+        self.y=y # y标签
+        self.w = np.random.randn(len(x[0]),1) # 根据特征的数量，生成w
+        for i in self.iter: # 遍历每一次迭代
+            self.step() 
+            self.loss_arr.append(self.loss()) #添加损失函数
+
+    def f(self,x):
+        return x *self.w+self.b
+
+    def predict(self):
+        y_pred = self.f(self.x)
+        return y_pred
+        
+    def mean_reduce(self,pre,label):
+        if len(pre) != len(label):
+            raise "pre length is not equal label length"
+        sum = 0
+        for i in range(len(pre)):
+            sum+=math.sqrt((pre[i] - label[i])^2)
+        return sum
+                 
+    def loss(self):
+        loss_value = mean_reduce(self.predict(),self.y)
+        return loss
+
+    def cal_gradient(self):
+        d_w = np.mean((self.w*self.x+self.b-self.y) * self.x)
+        d_b = np.mean((self.w*self.x+self.b-self.y))
+        return d_w,d_b
+
+    def step(self):
+        d_w,d_b = self.cal_gradient()
+        self.w = self.w - self.lr*d_w
+        self.b = self.b - self.lr*d_b
+        return self.w, self.b
+
+
+    
+
+    
+
+    
+        
